f9a9e2118c28459f10329c922f579851b5fc05e9
Abhishek Grover <Abhishek.Grover@smartenergywater.in>
2023-07-07 14:42:11 +0530

    changes

--- src/components/LeaveManagement/EssLeave.js
+++ src/components/LeaveManagement/EssLeave.js
@@ -1,85 +1,14 @@
 import React, { useState, useMemo } from "react";
 import { BiDotsVertical } from "react-icons/bi";
-import { useTable } from "react-table";
+import { quotaData, holidayData, lHistoryData } from "./Table";
 import "./EssLeave.css";
+import Table from "./Table";
 
 const EssLeave = () => {
   const [earnedL, setEarnedL] = useState("Earned Leave");
   const [quotaState, setQuotaState] = useState(1);
-  console.log(quotaState);
 
-  const quotaData = [
-    {
-      id: 1,
-      type: "Earned Leave",
-      total: 16.91,
-      used: 0,
-      actual: 16.91,
-      onHold: 0,
-      available: 4.5,
-      carryForward: 0,
-    },
-    {
-      id: 2,
-      type: "Finished Leave",
-      total: 26.91,
-      used: 4,
-      actual: 26.91,
-      onHold: 2,
-      available: 4.5,
-      carryForward: 0,
-    },
-    {
-      id: 3,
-      type: "Not Earned",
-      total: 4.9,
-      used: 2,
-      actual: 12.91,
-      onHold: 1,
-      available: 4.5,
-      carryForward: 0,
-    },
-  ];
-  const holidayData = [
-    {
-      name: "New Years Day",
-      from: "01-Jan-2023",
-      to: "01-Jan-2023",
-      days: 1,
-    },
-    {
-      name: "Makar Sakranti",
-      from: "14-Jan-2023",
-      to: "14-Jan-2023",
-      days: 1,
-    },
-    {
-      name: "Republic day",
-      from: "26-Jan-2023",
-      to: "26-Jan-2023",
-      days: 1,
-    },
-    {
-      name: "Good Friday",
-      from: "02-Apr-2023",
-      to: "02-Apr-2023",
-      days: 1,
-    },
-    {
-      name: "Ugadi",
-      from: "13-Apr-2023",
-      to: "13-Apr-2023",
-      days: 1,
-    },
-    {
-      name: "Shree Ram Navmi",
-      from: "21-Apr-2023",
-      to: "21-Apr-2023",
-      days: 1,
-    },
-  ];
-
-  const columns = useMemo(
+  const columns1 = useMemo(
     () => [
       {
         Header: "Leave Type",
@@ -108,7 +37,8 @@ const EssLeave = () => {
     ],
     []
   );
-  const holidayColumn = useMemo(
+  
+  const columns2 = useMemo(
     () => [
       {
         Header: "Holiday Name",
@@ -125,55 +55,10 @@ const EssLeave = () => {
       {
         Header: "No. of Days",
         accessor: "days",
-      },
+      }
     ],
     []
   );
-  const data = useMemo(() => quotaData, []);
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, holidayColumn, data });
-
-  const lHistoryData = [
-    {
-      id: 1,
-      duration: "28-Dec-2020 To 29-Dec-2020",
-      applied: "24_dec-2020",
-      days: "2",
-      status: "Approved",
-      color: "#119a4c",
-    },
-    {
-      id: 2,
-      duration: "28-Dec-2020 To 29-Dec-2020",
-      applied: "24_dec-2020",
-      days: "2",
-      status: "Cancelled",
-      color: "#ff9696",
-    },
-    {
-      id: 3,
-      duration: "28-Dec-2020 To 29-Dec-2020",
-      applied: "24_dec-2020",
-      days: "2",
-      status: "Approved",
-      color: "#119a4c",
-    },
-    {
-      id: 4,
-      duration: "28-Dec-2020 To 29-Dec-2020",
-      applied: "24_dec-2020",
-      days: "2",
-      status: "Cancelled",
-      color: "#ff9696",
-    },
-    {
-      id: 5,
-      duration: "28-Dec-2020 To 29-Dec-2020",
-      applied: "24_dec-2020",
-      days: "2",
-      status: "Approved",
-      color: "#119a4c",
-    },
-  ];
 
   const lHistoryElement =
     lHistoryData &&
@@ -207,7 +92,7 @@ const EssLeave = () => {
       <div className="container">
         <section className="left-side">
           <div className="lHistory-tag">
-            <h3 style={{ color: "#5db2ff", flex: "1" }}>Leave History</h3>
+            <h3 style={{ color: "#2670c5", flex: "1" }}>Leave History</h3>
             <div style={{ display: "flex", gap: "1rem", alignItems: "center" }}>
               <select className="earned-leave" onChange={(e) => setEarnedL(e.target.value)}>
                 <option value="Earned Leave">Earned Leave</option>
@@ -224,9 +109,15 @@ const EssLeave = () => {
         </section>
         <div className="right-side">
           <div className="quota">
-            <li className={quotaState===1 ? "quota-active" : "quota-li"} onClick={() => setQuotaState(1)}>Leave Quota</li>
-            <li className={quotaState===2 ? "quota-active" : "quota-li"} onClick={() => setQuotaState(2)}>Holiday List</li>
-            <li className={quotaState===3 ? "quota-active" : "quota-li"} onClick={() => setQuotaState(3)}>Optional Holidays</li>
+            <li className={quotaState === 1 ? "quota-active" : "quota-li"} onClick={() => setQuotaState(1)}>
+              Leave Quota
+            </li>
+            <li className={quotaState === 2 ? "quota-active" : "quota-li"} onClick={() => setQuotaState(2)}>
+              Holiday List
+            </li>
+            <li className={quotaState === 3 ? "quota-active" : "quota-li"} onClick={() => setQuotaState(3)}>
+              Optional Holidays
+            </li>
           </div>
           <div className="table-div">
             {quotaState === 1 && (
@@ -240,34 +131,7 @@ const EssLeave = () => {
                   </select>
                 </div>
                 <p style={{ fontSize: ".9rem", padding: "7px" }}>Can Carry Forward to Next Year</p>
-                <table {...getTableProps()} className="table">
-                  <thead className="thead">
-                    {headerGroups.map((headerGroup) => (
-                      <tr {...headerGroup.getHeaderGroupProps()} className="tr">
-                        {headerGroup.headers.map((column) => (
-                          <th {...column.getHeaderProps()} className="th">
-                            {column.render("Header")}
-                          </th>
-                        ))}
-                      </tr>
-                    ))}
-                  </thead>
-                  <tbody {...getTableBodyProps()}>
-                    {rows.map((row) => {
-                      prepareRow(row);
-                      return (
-                        <tr {...row.getRowProps()}>
-                          {row.cells.map((cell) => (
-                            <td {...cell.getCellProps()} className="td">
-                              {" "}
-                              {cell.render("Cell")}{" "}
-                            </td>
-                          ))}
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </table>
+                <Table columns={columns1} data={quotaData} />
               </>
             )}
             {quotaState === 2 && (
@@ -280,33 +144,7 @@ const EssLeave = () => {
                     <option>India General :: Holidays</option>
                   </select>
                 </div>
-                <table {...getTableProps()} className="table">
-                  <thead className="thead">
-                    {headerGroups.map((headerGroup) => (
-                      <tr {...headerGroup.getHeaderGroupProps()} className="tr">
-                        {headerGroup.headers.map((column) => (
-                          <th {...column.getHeaderProps()} className="th">
-                            {column.render("Header")}
-                          </th>
-                        ))}
-                      </tr>
-                    ))}
-                  </thead>
-                  <tbody {...getTableBodyProps()}>
-                    {rows.map((row) => {
-                      prepareRow(row);
-                      return (
-                        <tr {...row.getRowProps()}>
-                          {row.cells.map((cell) => (
-                            <td {...cell.getCellProps()} className="td">
-                              {cell.render("Cell")}
-                            </td>
-                          ))}
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </table>
+                <Table columns={columns2} data={holidayData} />
               </>
             )}
             {quotaState === 3 && (
@@ -319,34 +157,11 @@ const EssLeave = () => {
                     <option>India Holidays List 2025</option>
                   </select>
                 </div>
-                <table {...getTableProps()} className="table">
-                  <thead className="thead">
-                    {headerGroups.map((headerGroup) => (
-                      <tr {...headerGroup.getHeaderGroupProps()} className="tr">
-                        {headerGroup.headers.map((column) => (
-                          <th {...column.getHeaderProps()} className="th">
-                            {column.render("Header")}
-                          </th>
-                        ))}
-                      </tr>
-                    ))}
-                  </thead>
-                  <tbody {...getTableBodyProps()}>
-                    {rows.map((row) => {
-                      prepareRow(row);
-                      return (
-                        <tr {...row.getRowProps()}>
-                          {row.cells.map((cell) => (
-                            <td {...cell.getCellProps()} className="td">
-                              {cell.render("Cell")}
-                            </td>
-                          ))}
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </table>
-                <p style={{color: "#999999", fontSize: ".8rem", lineHeight: "1.7rem", marginTop: "1rem"}}>You cannot opt optional holidays at this moment. Please contact your HR.<br /> <span style={{color: "#666666", fontWeight: "bold"}}>Note:</span>This is one time selection. Once opted you can not change</p>
+                <Table columns={columns2} data={holidayData} />
+                <p style={{ color: "#999999", fontSize: ".8rem", lineHeight: "1.7rem", marginTop: "1rem" }}>
+                  You cannot opt optional holidays at this moment. Please contact your HR.
+                  <br /> <span style={{ color: "#666666", fontWeight: "bold" }}>Note:</span>This is one time selection. Once opted you can not change
+                </p>
               </>
             )}
           </div>
---
+++ src/components/LeaveManagement/Table.js
@@ -0,0 +1,150 @@
+import React, { useMemo } from "react";
+import { useTable } from "react-table";
+import "./EssLeave.css";
+
+
+  
+  const Table = ({ columns, data }) => {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
+    columns,
+    data,
+  });
+
+
+  return (
+    <table {...getTableProps()} className="table">
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()} className="tr">
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()} className="th">
+                {column.render("Header")}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <td {...cell.getCellProps()} className="td">
+                  {cell.render("Cell")}
+                </td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
+
+export const quotaData = [
+    {
+      id: 1,
+      type: "Earned Leave",
+      total: 16.91,
+      used: 0,
+      actual: 16.91,
+      onHold: 0,
+      available: 4.5,
+      carryForward: 0,
+    },
+    {
+      id: 2,
+      type: "Finished Leave",
+      total: 26.91,
+      used: 4,
+      actual: 26.91,
+      onHold: 2,
+      available: 4.5,
+      carryForward: 0,
+    },
+    {
+      id: 3,
+      type: "Not Earned",
+      total: 4.9,
+      used: 2,
+      actual: 12.91,
+      onHold: 1,
+      available: 4.5,
+      carryForward: 0,
+    },
+  ]; 
+export const holidayData = [
+    {
+        ids: 1,
+        name: "New Years Day",
+        from: "01-Jan-2023",
+        to: "01-Jan-2023",
+        days: 1,
+    },
+    {
+        ids: 2,
+        name: "Makar Sakranti",
+        from: "14-Jan-2023",
+        to: "14-Jan-2023",
+        days: 1,
+    },
+    {
+        ids: 3,
+        name: "Republic day",
+        from: "26-Jan-2023",
+        to: "26-Jan-2023",
+        days: 1,
+    },
+    {
+        ids: 4,
+        name: "Good Friday",
+        from: "02-Apr-2023",
+        to: "02-Apr-2023",
+        days: 1,
+    },
+];
+export const lHistoryData = [
+    {
+      id: 1,
+      duration: "28-Dec-2020 To 29-Dec-2020",
+      applied: "24-dec-2020",
+      days: "2",
+      status: "Approved",
+      color: "#119a4c",
+    },
+    {
+      id: 2,
+      duration: "28-Dec-2020 To 29-Dec-2020",
+      applied: "24-dec-2020",
+      days: "2",
+      status: "Cancelled",
+      color: "#ff9696",
+    },
+    {
+      id: 3,
+      duration: "28-Dec-2020 To 29-Dec-2020",
+      applied: "24-dec-2020",
+      days: "2",
+      status: "Approved",
+      color: "#119a4c",
+    },
+    {
+      id: 4,
+      duration: "28-Dec-2020 To 29-Dec-2020",
+      applied: "24-dec-2020",
+      days: "2",
+      status: "Cancelled",
+      color: "#ff9696",
+    },
+    {
+      id: 5,
+      duration: "28-Dec-2020 To 29-Dec-2020",
+      applied: "24-dec-2020",
+      days: "2",
+      status: "Approved",
+      color: "#119a4c",
+    },
+  ];
